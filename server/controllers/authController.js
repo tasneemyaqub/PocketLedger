7e6b3ec89313ffcb2705ad966c5c534aa9365e77
tasneemyaqub <tasneemyaqub@gmail.com>
2026-05-23 15:34:13 +0530

    implemented login and tokens (initial)

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -30,4 +30,51 @@ const registerUser = async (req, res) => {
     }
 };
 
-module.exports = {registerUser};
+const loginUser = async (req, res) => {
+    try {
+        const { mobile, password } = req.body;
+        const user = await User.findOne({mobile});
+
+        if(!user) {
+            return res.status(400). json({
+                message: "Invalid credentials",
+            });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).json({
+                message: "Invalid credentials",
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d",
+            }
+        );
+
+        res.status(200).json({
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                mobile: user.mobile,
+            },
+        });
+    } catch(error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+module.exports = {
+    registerUser,
+    loginUser,
+};
